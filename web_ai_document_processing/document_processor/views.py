f9e8694767ad7a0cdbdc8fb350d6c1e13b32bdb0
Norbert <mainanorbert@gmail.com>
2026-02-05 20:52:41 +0300

    properly implemented the cookies

--- web_ai_document_processing/document_processor/views.py
+++ web_ai_document_processing/document_processor/views.py
@@ -10,27 +10,6 @@ from document_processor.services.llm import run_llm_document_extraction
 
 from django.views.decorators.csrf import csrf_exempt
 
-# @csrf_exempt
-# @require_POST
-# def process_document(request):
-#     """
-#     Accept an uploaded image and return extracted data.
-#     """
-#     uploaded_file = request.FILES.get("document")
-
-#     if not uploaded_file:
-#         return JsonResponse({"error": "No file uploaded"}, status=400)
-
-#     file_path = default_storage.save(
-#         f"uploads/{uploaded_file.name}", uploaded_file
-#     )
-#     print(f"File saved to: {file_path}")
-
-#     absolute_path = os.path.join(default_storage.location, file_path)
-
-#     result = run_llm_document_extraction(image_path=absolute_path)
-
-#     return JsonResponse(result, status=200)
 
 @require_POST
 def process_document(request):
